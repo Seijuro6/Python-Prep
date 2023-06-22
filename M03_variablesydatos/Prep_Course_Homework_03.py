62c6e81797a2cbbfc0866f1f5e9786817441ad41
Harold Cadena <astarot6.6@hotmail.com>
2023-06-22 13:53:57 -0500

    homework

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,7 +7,8 @@
 
 # In[7]:
 
-
+x=8
+print(x)
 
 
 # 2) Imprimir el tipo de dato de la constante 8.5
@@ -15,14 +16,14 @@
 # In[3]:
 
 
-
+type(8.5)
 
 
 # 3) Imprimir el tipo de dato de la variable creada en el punto 1
 
 # In[8]:
 
-
+print ( 'el tipo de dato que es x es', type(x))
 
 
 
@@ -30,14 +31,14 @@
 
 # In[2]:
 
-
+nombre = 'Harold'
 
 
 # 5) Crear una variable que contenga un número complejo
 
 # In[3]:
 
-
+complejo = 5 + 5j
 
 
 
@@ -46,7 +47,7 @@
 # In[4]:
 
 
-
+type(complejo)
 
 
 # 7) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
@@ -61,7 +62,8 @@ pi = 3.1416
 
 # In[3]:
 
-
+valor1 = 'True'
+valor2 = True
 
 
 
@@ -70,14 +72,14 @@ pi = 3.1416
 # In[5]:
 
 
-
+print('el valor1 es de tipo ',  type(valor1),  'el valor2 es de tipo ',  type(valor2) )
 
 
 # 10) Asignar a una variable, la suma de un número entero y otro decimal
 
 # In[1]:
 
-
+suma = 8 + 2.5
 
 
 
@@ -85,7 +87,9 @@ pi = 3.1416
 
 # In[2]:
 
-
+a = 1+3j
+b = 3+3j
+print(a+b)
 
 
 
@@ -94,7 +98,8 @@ pi = 3.1416
 # In[4]:
 
 
-
+c = a + 1.1
+print(c)
 
 
 # 13) Realizar una operación de multiplicación
@@ -102,14 +107,14 @@ pi = 3.1416
 # In[5]:
 
 
-
+print(2*6)
 
 
 # 14) Mostrar el resultado de elevar 2 a la octava potencia
 
 # In[6]:
 
-
+print(2**8)
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
@@ -117,7 +122,8 @@ pi = 3.1416
 # In[8]:
 
 
-
+d = 27/4
+print(d)
 
 
 # 16) De la división anterior solamente mostrar la parte entera
@@ -125,7 +131,8 @@ pi = 3.1416
 # In[9]:
 
 
-
+e = 27//4
+print (e)
 
 
 # 17) De la división de 27 entre 4 mostrar solamente el resto
@@ -133,7 +140,8 @@ pi = 3.1416
 # In[1]:
 
 
-
+f = 27%4
+print(f)
 
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
@@ -141,7 +149,8 @@ pi = 3.1416
 # In[2]:
 
 
-
+g= e*4+f
+print(g)
 
 
 # 19) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
@@ -149,7 +158,9 @@ pi = 3.1416
 # In[3]:
 
 
-
+h = 'Hola '
+i = 'Mundo '
+print(h +i)
 
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
@@ -157,7 +168,7 @@ pi = 3.1416
 # In[4]:
 
 
-
+print(2=='2')
 
 
 # 21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
@@ -165,14 +176,14 @@ pi = 3.1416
 # In[11]:
 
 
-
+2 == int('2')
 
 
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
 
 # In[12]:
 
-
+j = float('3,8')
 
 
 
@@ -181,7 +192,9 @@ pi = 3.1416
 # In[15]:
 
 
-
+k = 3
+k-=1
+print(k) 
 
 
 # 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
@@ -189,15 +202,17 @@ pi = 3.1416
 # In[29]:
 
 
-
-
+1<<2
 
 # 25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
 
 # In[23]:
 
 
-
+2+'2'
+float(2)+float('2')
+int(2)+int('2')
+str(2)+str('2')
 
 
 
@@ -205,5 +220,7 @@ pi = 3.1416
 
 # In[30]:
 
-
+var1 = 'este texto se repite '
+var2 = 3
+print(var1 * var2 + str(var2) + ' veces')
 
